fc89a944a39f94d73a2517cd9ab8f519e0c2f417
Gustavo <gustavoplensack@gmail.com>
2020-08-04 09:12:20 -0300

    Add repos html and css

--- src/pages/Repsoitory/index.tsx
+++ src/pages/Repsoitory/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom';
+import {useRouteMatch, Link} from 'react-router-dom';
+import {FiChevronLeft} from 'react-icons/fi'
+
+import {Header,RepositoryInfo} from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
     repo: string;
@@ -9,7 +14,41 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const {params} = useRouteMatch<RepositoryParams>()
 
-return <h1>Repository</h1>;
+return (
+    <>
+        <Header>
+            <img src={logoImg} alt="GitHub Explorer"/>
+            <Link to="/">
+                <FiChevronLeft size={16}/>
+                Voltar
+            </Link>
+        </Header>
+
+        <RepositoryInfo>
+            <header>
+                <img src="https://avatars1.githubusercontent.com/u/15476059?s=460&u=03269b24a681e8d63ef8ae1b56a2143032ba3625&v=4" alt="gustavoplensack"/>
+                <div>
+                    <strong>gustavoplensack/github-explorer</strong>
+                    <p>Descrição do Repo</p>
+                </div>
+            </header>
+            <ul>
+                <li>
+                    <strong>200</strong>
+                    <p>Stars</p>
+                </li>
+                <li>
+                    <strong>100</strong>
+                    <p>Forks</p>
+                </li>
+                <li>
+                    <strong>50</strong>
+                    <p>Issues</p>
+                </li>
+            </ul>
+        </RepositoryInfo>
+    </>
+);
 };
 
 export default Repository;
